1feb89a8467069997cc52d0d807ded3d20515f1c
iyokurot <01kayochin17neko@gmail.com>
2019-10-01 06:12:59 +0900

    express接続&ローカル動画読み込み

--- Assets/Script/ServerTest.cs
+++ Assets/Script/ServerTest.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class ServerTest : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class ServerTest : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+        StartCoroutine (GetServerData ());
+
+        StartCoroutine (PostServerData ());
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+
+    }
+    //Get通信テスト
+    IEnumerator GetServerData () {
+        UnityWebRequest req = UnityWebRequest.Get ("http://localhost:3000/Get");
+        req.SetRequestHeader ("key", "KEY");
+        yield return req.Send ();
+        if (req.isError) {
+            Debug.Log (req.error);
+        } else {
+            if (req.responseCode == 200) {
+                //OK
+                string jsonText = req.downloadHandler.text;
+                List<Testdata> list = new List<Testdata> ();
+                list = JsonUtility.FromJson<Serialize<Testdata>> (jsonText).ToList ();
+                Debug.Log (list[0].name);
+            }
+        }
+    }
+    //Post通信テスト
+    IEnumerator PostServerData () {
+        WWWForm form = new WWWForm ();
+        //key:data
+        form.AddField ("myField", "myData");
+        form.AddField ("newxt", 20);
+
+        using (UnityWebRequest req = UnityWebRequest.Post ("http://localhost:3000/Post", form)) {
+            yield return req.Send ();
+
+            if (req.isError) {
+                Debug.Log (req.error);
+            } else {
+                Debug.Log ("Form upload complete!");
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class Testdata {
+        public string id;
+        public string name;
     }
 }
